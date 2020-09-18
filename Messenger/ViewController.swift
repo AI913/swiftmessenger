185ba3b9d6f37fa45a97bb065ad1bc01017a5948
AI913 <dywchiu913@hotmail.com>
2020-09-18 14:31:58 +0900

    Getting started

---
+++ Messenger/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Messenger
+//
+//  Created by 招耀華 on 18/9/2020.
+//  Copyright © 2020 招耀華. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
